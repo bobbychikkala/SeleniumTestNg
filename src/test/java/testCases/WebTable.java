ab13d08a141d87e697c8f8e5d7883ce448fc2230
bobbychikkala <chikkalabobbyk2@gmail.com>
2025-06-20 15:29:12 +0530

    changes

--- src/test/java/testCases/WebTable.java
+++ src/test/java/testCases/WebTable.java
@@ -8,47 +8,63 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 public class WebTable {
 	static WebDriver driver = new ChromeDriver();
+	static String innings;
+	
+	@DataProvider(name="inningNo")
+	public  String[][]dataSet(){
+		String url1="https://www.cricbuzz.com/live-cricket-scorecard/106673/rsa-vs-aus-final-icc-world-test-championship-final-2025";
+		String url2="https://www.cricbuzz.com/live-cricket-scorecard/118586/sl-vs-ban-1st-test-bangladesh-tour-of-sri-lanka-2025";
+		return   new String[][]{
+			{"innings_1",url1},
+			{"innings_2",url1},
+			{"innings_3",url1},
+			{"innings_4",url1},
+			{"innings_1",url2},
+			{"innings_2",url2},
+			{"innings_3",url2},
+			};
+	}
 
-	@Test
-	public static void testChampionFinal() {
-		String scoreCard = "https://www.cricbuzz.com/live-cricket-scorecard/106673/rsa-vs-aus-final-icc-world-test-championship-final-2025";
-		driver.get(scoreCard);
-	}
-	@Test
-	public static void banVsSl() {
-		String scoreCard = "https://www.cricbuzz.com/live-cricket-scorecard/118586/sl-vs-ban-1st-test-bangladesh-tour-of-sri-lanka-2025";
-		driver.get(scoreCard);
-	}
-	@AfterMethod
-	public void testDynamicTable() {
+	@Test(dataProvider ="inningNo",priority=1)
+	public  void testCricketScore(String inning,String url) {
+		driver.get(url);
+		innings=inning;
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		int batsmen = driver.findElements(By.xpath("//div[@id='innings_1']/div[1]/div/div[1]/a")).size();
-		int column = driver.findElements(By.xpath("(//div[@id='innings_1']/div[1]/div[3]/div)")).size();
+		String commonXpath ="//div[@id='"+innings+"']/div[1]";
+		int batsmen = driver.findElements(By.xpath("//div[@id='"+innings+"']/div[1]/div/div[1]/a")).size();
+		int column = driver.findElements(By.xpath("//div[@id='"+innings+"']/div[1]/div[3]/div")).size();
 
-		for (int r=0 ; r<batsmen+1;r++) {		
+		for (int r=2 ; r<=batsmen+2;r++) {		
 			for(int c=1;c<column;c++) {
-				int k=2+r;
-				String data =driver.findElement(By.xpath("(//div[@id='innings_1']/div[1]/div["+k+"]/div)["+c+"]")).getText();
+				
+				String data =driver.findElement(By.xpath(commonXpath+"/div["+r+"]/div["+c+"]")).getText();
 				System.out.print(data);
-				if((c<2) && data.length()<25){
-					for(int e=0;e<25-data.length();e++)System.out.print(" ");
-				}
-				if((c==2) && data.length()<45){
-					for(int d=0;d<45-data.length();d++)System.out.print(" ");
-				}
-				if((c>2) && data.length()<5){
-					for(int e=0;e<5-data.length();e++)System.out.print(" ");
-				}
+				
+				if(c<2) for(int e=0;e<29-data.length();e++)System.out.print(" ");
+				
+				else if(c==2) for(int d=0;d<45-data.length();d++)System.out.print(" ");
+		
+				else  for(int e=0;e<6-data.length();e++)System.out.print(" ");
+				
 			}
 			System.out.println();
 		}
 		Assert.assertTrue(true);
 		//driver.quit();
 		System.out.println("===========================================================================================================");
+	
 	}
+	
+	
+	@AfterTest
+	public void closeBrowser() {
+		driver.quit();
+	}
+	
+	
 }
